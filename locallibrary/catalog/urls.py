b9ab31c1c11ee57b93dddd20ad59a2d0d41f8aea
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-20 21:59:59 -0500

    corrigiendo URL pattern'/'

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Define las URL aquí utilizando la función path()
-    # Ejemplo: path('ruta/', views.mi_vista, name='nombre_vista'),
+    # Patrón de URL corregido sin barra diagonal inicial
+    path('', views.home, name='home'),  
+    # Otros patrones de URL aquí...
 ]
