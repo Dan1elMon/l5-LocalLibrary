9f6f9b980f113cbad409a66a2df45e162364f815
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-20 23:10:32 -0500

    corrigiendo

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.urls import include
 from . import views
 
 
 urlpatterns = [
-    path('catalog/', include('catalog.urls')),
+    path('', views.index, name='index'),
+    # ...otras rutas de la aplicación catalog...
 ]
