df5513441c9220affd2a436ce26dadc36ce78af6
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-20 21:53:27 -0500

    corrigiendo

--- locallibrary/catalog/catalog.py
+++ locallibrary/catalog/catalog.py
@@ -6,10 +6,3 @@ from . import views
 urlpatterns = [
 
 ]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Define las URL aquí utilizando la función path() o re_path()
-    # Ejemplo: path('ruta/', views.mi_vista, name='nombre_vista'),
-]
