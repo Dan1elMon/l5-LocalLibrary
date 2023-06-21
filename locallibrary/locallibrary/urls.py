b7be2657dc834d9f098d3977bcf7f4c44e5ec912
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-20 21:48:44 -0500

    revisando

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -38,8 +38,6 @@ urlpatterns += [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
