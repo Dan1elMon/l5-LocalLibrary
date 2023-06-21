4894803aacec3896f818ba83b5d1391eac14a1e4
Daniel Chura <dchuramo@unsa.edu.pe>
2023-06-20 22:05:25 -0500

    revisando

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
+
 urlpatterns = [
-    # Patrón de URL corregido sin barra diagonal inicial
-    path('', views.home, name='home'),  
-    # Otros patrones de URL aquí...
+
 ]
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -31,15 +31,11 @@ urlpatterns += [
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('/', RedirectView.as_view(url='/catalog/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
